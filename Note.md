43c103c3be43c5d9d65012c8abb6275a57a07575
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-02-20 21:16:41 +0700

    add customers table migration

---
+++ Note.md
@@ -0,0 +1,6 @@
+Used Tables
+-----------
+
+customers table with structure:
+
+![Customer Table Structure](./docs/image/customers_table_structure.png)
---
+++ database/migrations/2020_02_20_134648_create_customers_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 20);
+            $table->string('name', 150);
+            $table->string('email', 50);
+            $table->mediumText('address');
+            $table->date('date_of_birth');
+            $table->string('city', 50);
+            $table->string('region', 50);
+            $table->string('postcode', 10);
+            $table->string('country_code', 2);
+            $table->string('telephone', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
