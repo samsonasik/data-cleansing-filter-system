82434428241e96eeb255d74fcb0d61ece7ae1c7c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-02-21 07:04:22 +0700

    report label

--- resources/views/system/report.blade.php
+++ resources/views/system/report.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Form</div>
+                <div class="card-header">Report</div>
 
                 <div class="card-body">
                     @if (session('status'))
