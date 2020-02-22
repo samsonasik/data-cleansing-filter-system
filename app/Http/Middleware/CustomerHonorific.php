21b2e2a69621fbd2a3cda867b5af5a92878ab059
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-02-22 07:09:28 +0700

    clear data

--- app/Http/Middleware/CustomerHonorific.php
+++ app/Http/Middleware/CustomerHonorific.php
@@ -35,7 +35,7 @@ class CustomerHonorific
                 continue;
             }
 
-            $datacleansing[$customer->id]['title'] = sprintf('title is invalid, use %s, or %s',
+            $datacleansing[$customer->id]['title'] = sprintf('title is invalid, you can set it empty, or %s, or %s',
                 self::ALLOWED_HONORIFIC[1],
                 self::ALLOWED_HONORIFIC[2]
             );
