8c0184ab581c1e5698145421f744a06cf62e98ae
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-02-22 08:37:45 +0700

    unit test for CustomerTitle when score already exists, increment

---
+++ tests/Unit/Middleware/CustomerTitleTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Unit\Middleware;
+
+use App\Http\Middleware\CustomerTitle;
+use Illuminate\Http\Request;
+use PHPUnit\Framework\TestCase;
+
+class CustomerTitleTest extends TestCase
+{
+    private $middleware;
+
+    protected function setUp(): void
+    {
+        $this->middleware = new CustomerTitle();
+    }
+
+    public function testHandleScoreAlreadyExistIncrementExisting()
+    {
+        $request = new Request();
+        $request->attributes->set('datacleansing', [
+            1 => [
+                'other_filter' => 'abc',
+                'score' => 10,
+            ]
+        ]);
+        $request->attributes->set('customers', [
+            (object) [
+                'id' => 1,
+                'title' => 'XYZ'
+            ],
+        ]);
+
+        $this->middleware->handle($request, function () { });
+        $this->assertEquals(20, $request->attributes->get('datacleansing')[1]['score']);
+    }
+}
